83f3e2ad8659871a9d18b7a7716d8880ec21a991
GeovaniTuz <43384721+GeovaniTuz@users.noreply.github.com>
2020-03-19 21:06:08 -0600

    Función 1

--- js/geraTempo.js
+++ js/geraTempo.js
@@ -1,33 +1,16 @@
-function temporal() {
-    /*
-    puntos importantes:
 
-      1.-no se recomienda usar var en su lugar usar let
-      2.- prompt se usa para pedir datos.
-      3.-` Numero ${dato} de 9` --> Numero 9 de 9
-    */
+function Ecu0() {
 
-    //se pide datos en pantalla html
-    let datos = prompt("Introducir dato para Evaluar")
-
-    // se compreva los datos introducidos.
-    console.log(datos);
-
-    let suma = new RegExp('[0-9"+"0-9]', 'g');
-
-    let nul = datos.match(suma);//agarra todo lo que encuentre con suma
-
-    let lineo =  "<br>";// para salto de linea
-
-    //se inicia el for para generar los temporales
-    for (let i = 0; i<5; i++){// el valor 5 se cambiara segun la posicion a la que pertenesca
-        console.log(`t${i} = ${nul}`);// genera datos para que se muestre en consola
-        let tempos = `t${i} = ${nul}`;// aqui se alamacena el mensage necesario ``(comillas graves)
-        document.write(lineo, tempos);// lo que contenga linde o tempos lo manda a otra ventana con los resultados.
-
-    }
-    
-    
-
-
+    let ecuacion = document.getElementById('ecu0').value;
+    if (ecuacion != null) {
+        let expres = /\d{1,}\s{0,}['+']\s{0,1}\d{1,}/gi;
+        let suma = ecuacion.match(expres);
+        let i = 0;
+        let t = 't';
+        let result = t + i + ' = ' + suma;
+        let otro = ecuacion.replace(expres, t + i);
+        i++;
+        let result2 = t + i + ' = ' + otro;
+        alert(`Operacion: ${ecuacion} \n \n  ${result} \n ${result2}`)
+    };
 }
