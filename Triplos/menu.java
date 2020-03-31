6d065b6b479322cc9420ae343a85154365849a79
jesus121299 <guadalupe.maykauil@itsva.edu.mx>
2020-03-31 13:46:23 -0600

    segunda revision

---
+++ Triplos/menu.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Triplos;
+
+/**
+ *
+ * @author Geovani Alberto
+ */
+public class menu {
+    public static void main(String[] args) {
+        // menu principal
+        Interfaz menu = new Interfaz();
+        menu.setTitle("Tripos");
+        menu.setSize(400, 250);
+        menu.setVisible(true);
+       
+
+    }
+
+}
